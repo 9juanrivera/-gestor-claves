39a3d4ec4edff249642215363d30ba3996244cee
9juanrivera <9juanrivera@gmail.com>
2026-06-23 12:20:32 -0500

    fix db port

--- src/db.js
+++ src/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
-require('dotenv').config();
 
 const conexion = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306
 });
 
 conexion.connect((error) => {
